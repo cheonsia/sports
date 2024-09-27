f3a7369ebbc1954873d70041fd45802e09f6429e
sia cheon <jysa0512@naver.com>
2024-09-27 15:21:27 +0900

    csa : 1.1 수정 - baseball java

--- sports/src/main/java/com/mbc/sports/baseballplayer/BPlayerService.java
+++ sports/src/main/java/com/mbc/sports/baseballplayer/BPlayerService.java
@@ -4,9 +4,45 @@ import java.util.ArrayList;
 
 public interface BPlayerService {
 
+	void playersoccer(String tname, String pname, int pnumber, String pbirth, int height, int weight, String main,
+			String pimage, String pchar, String play);
+
+	void playerbaseball(String tname, String pname, int pnumber, String pbirth, int height, int weight, String main,
+			String pimage, String pchar, String play);
+
+	ArrayList<BPlayerDTO> selectTeam(String tname);
+
+	BPlayerDTO detail(int playernum);
+
+	void clickup(int pnumber);
+
+	ArrayList<BUcommentDTO> comment(int pnumber);
+
+	void commentin(int pnumber, String writer, String comment, int step);
+
+	int setStep(int playernum);
+
+	void updateset(int step, int playernum);
+
+	void heart(int playernum, String writer, String ucomment);
+
 	ArrayList<BPlayerDTO> allTeamSelect();
 
-	ArrayList<BPlayerDTO> selectTeam(String btname);
+	BPlayerDTO playerdelete(int playernum);
+
+	void delete(int playernum);
+
+	void playerbaseballupdate(String tname, String pname, int pnumber, String pbirth, int height, int weight,
+			String main, String pimage, String pchar, int playernum);
+
+	void playersoccerupdate(String tname, String pname, int pnumber, String pbirth, int height, int weight, String main,
+			String pimage, String pchar, int playernum);
+
+	void commentdelete(int playernum, int step);
+
+	void commentupdate(int playernum, int step, String ucomment);
+
+	ArrayList<BPlayerDTO> brandom();
 	
 
 
---
+++ sports/src/main/java/com/mbc/sports/baseballplayer/BUcommentDTO.java
@@ -0,0 +1,101 @@
+package com.mbc.sports.baseballplayer;
+
+public class BUcommentDTO {
+	int playernum,step,indent;
+	String writer,ucomment,cdate;
+	int heart;
+
+	public BUcommentDTO() {}
+
+	
+
+
+	public int getHeart() {
+		return heart;
+	}
+
+
+
+
+	public void setHeart(int heart) {
+		this.heart = heart;
+	}
+
+
+
+
+	public String getWriter() {
+		return writer;
+	}
+
+
+
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+
+
+
+	public String getUcomment() {
+		return ucomment;
+	}
+
+
+
+	public void setUcomment(String ucomment) {
+		this.ucomment = ucomment;
+	}
+
+
+
+	public String getCdate() {
+		return cdate;
+	}
+
+
+
+	public void setCdate(String cdate) {
+		this.cdate = cdate;
+	}
+
+
+
+
+	public int getPlayernum() {
+		return playernum;
+	}
+
+
+
+	public void setPlayernum(int playernum) {
+		this.playernum = playernum;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
+
+	public int getIndent() {
+		return indent;
+	}
+
+	public void setIndent(int indent) {
+		this.indent = indent;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "UcommentDTO [playernum=" + playernum + ", step=" + step + ", indent=" + indent + ", writer=" + writer
+				+ ", ucomment=" + ucomment + ", cdate=" + cdate + "]";
+	}
+	
+}
