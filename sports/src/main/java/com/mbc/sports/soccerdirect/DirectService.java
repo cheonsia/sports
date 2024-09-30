4b55d73384be4c9d4b956f25bc8c224c6fe08395
hyerin lee <helena1005@naver.com>
2024-09-30 09:07:12 +0900

    lhr: 1.1 수정 - java

--- sports/src/main/java/com/mbc/sports/soccerdirect/DirectService.java
+++ sports/src/main/java/com/mbc/sports/soccerdirect/DirectService.java
@@ -4,4 +4,8 @@ import java.util.ArrayList;
 
 public interface DirectService {
 	ArrayList<DirectDTO> selectCalendarArea(String areaname1);
+	void soccercalendarsave(String team, String traning_select_date1, String traning_select_date2,
+			String traning_people_select, String traning_map_input, String chked_traing_val,
+			String training_memo_txtarea, String calendar_info_val);
+	ArrayList<DirectDTO> selectCalendarInfo(String areaname, String calendar_info);
 }
