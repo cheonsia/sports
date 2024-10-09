ec88f785eb6f28d4bb9527d445b0f9dfe8aa7ce8
kyuRi Kim <kkr980615@naver.com>
2024-10-10 02:29:44 +0900

    kkr: ver 2.0 통합-회원 관련 코드 간결화 및 수정(완)

--- sports/src/main/java/com/mbc/sports/login/LoginService.java
+++ sports/src/main/java/com/mbc/sports/login/LoginService.java
@@ -1,9 +1,13 @@
 package com.mbc.sports.login;
 
+import com.mbc.sports.member.MemberDTO;
+
 public interface LoginService {
 
-	LoginDTO logincheck(String id);
+	MemberDTO logincheck(String id);
 
 	String password(String id);
+
+	String access(String id);
 
 }
